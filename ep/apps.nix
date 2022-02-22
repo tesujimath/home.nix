884d8486f9b92127fc9e758cf51b6fb238f3faed
Simon Guest <sguest@educationperfect.com>
2022-02-22 17:03:20 +1300

    Add curlie

--- ep/apps.nix
+++ ep/apps.nix
@@ -4,6 +4,7 @@
   home.packages = [
     #pkgs.nosql-workbench # DynamoDB UI, not yet in NixOS channel
     pkgs.altair  # beautiful feature-rich GraphQL Client IDE
+    pkgs.curlie  # the power of curl, the ease of use of httpie
     pkgs.gh
   ];
 }
