c350ab940876324072ca328de855ffa8c8ce05de
Simon Guest <sguest@educationperfect.com>
2022-03-28 10:06:14 +1300

    Bump Rider to 2021.3.3

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -3,11 +3,11 @@ self: super:
   jetbrains = super.jetbrains // {
     rider = super.jetbrains.rider.overrideAttrs (_: rec {
       name = "rider-${version}";
-      version = "2021.3.1";
+      version = "2021.3.3";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "0c788xvcd5b9jafz2yyllj1pzgc9ry3pg82qi8glghvimjnk1cfd";
+        sha256 = "13q6hk5l3fqmz818z5wj014jd5iglpdcpi8zlpgaim1jg5fpvi8x";
       };
     });
   };
