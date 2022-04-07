06ea6030af8d17fa4447f4b8b2d4bfb107ff7b2d
Simon Guest <sguest@educationperfect.com>
2022-04-08 10:38:21 +1200

    Add earthly to ep-backend suite
    For containerized builds.

--- modules/ep-dev-backend/default.nix
+++ modules/ep-dev-backend/default.nix
@@ -6,14 +6,9 @@
 
     #pkgs.awless
     pkgs.awscli2
+    pkgs.earthly
     pkgs.jetbrains.datagrip
     pkgs.jetbrains.rider
     pkgs.python39Packages.mitmproxy
-
-    # these are needed for the building the server repo natively,
-    # but would be better as a shell.nix in that repo, or even just
-    # switching to building that with docker
-    pkgs.libgdiplus
-    pkgs.libmediainfo
   ];
 }
