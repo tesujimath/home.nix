f7d59a986e558a16fec463a14b23c200b8936dbf
Simon Guest <simon.guest@tesujimath.org>
2022-06-24 14:44:12 +1200

    Split into multiple configs
    sabaki$ home-manager switch -f ~/.config/nixpkgs/home.personal.nix
    
    aji$ home-manager switch -f ~/.config/nixpkgs/home.ep.nix

---
+++ home.personal.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./common.nix
+    #./modules/ep-dev-backend
+    ./modules/xmonad-desktop
+    ./packages.nix
+    #./ep/apps.nix
+    #./ep/aws.nix
+    #./secrets.personal.nix
+  ];
+}
---
+++ secrets.personal.nix
@@ -0,0 +1,14 @@
+{
+  imports = [ ./third-party/alexey/home/modules/secureEnv/onePassword.nix ];
+
+  config = {
+    secureEnv.onePassword = {
+      enable = true;
+      sessionVariables = {
+      };
+      sshKeys = {
+        # the IDs may be found from `op list items`
+      };
+    };
+  };
+}
