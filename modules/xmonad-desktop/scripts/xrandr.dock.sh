15adaa114582823e42c70cda8b19f47d6009228f
Simon Guest <simon.guest@tesujimath.org>
2023-01-20 09:17:01 +1300

    Remove dock/undock layouts from home manager
    Need to be able to have them local per host, not in this repo.

--- modules/xmonad-desktop/scripts/xrandr.dock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP --mode 1920x1200 --pos 5760x960 --rotate normal --output HDMI-A-0 --primary --mode 1920x1200 --pos 0x960 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --mode 3840x2160 --pos 1920x0 --rotate normal --output DisplayPort-8 --off
