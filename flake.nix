10c3636721320ff8adf87095b0aeb3dcdff06e23
Simon Guest <simon.guest@tesujimath.org>
2023-05-09 11:19:22 +1200

    Switch to master branch for compatability with nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "sjg home manager flake";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
     home-manager = {
-      url = github:nix-community/home-manager/release-22.11;
+      url = github:nix-community/home-manager/master;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
