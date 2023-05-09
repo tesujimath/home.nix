7a6bc138826f69629462ab9b362cdf2afb0612c9
Simon Guest <simon.guest@tesujimath.org>
2023-05-09 12:04:45 +1200

    direnv now managed by home.nix not underlying NixOS

--- common.nix
+++ common.nix
@@ -7,8 +7,15 @@
     ];
 
   config = {
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
+    programs = {
+      # Let Home Manager install and manage itself.
+      home-manager.enable = true;
+
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+    };
 
     # additional docs, access via home-manager-help command
     manual.html.enable = true;
