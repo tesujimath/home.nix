120c0dea98811c15624271d353e8fcfbb9a6ba41
Simon Guest <simon.guest@tesujimath.org>
2023-06-06 15:27:52 +1200

    Helix disable arrow keys and up/down in insert mode

--- modules/helix.nix
+++ modules/helix.nix
@@ -21,6 +21,18 @@ with pkgs;
               enable = true;
             };
           };
+          keys = {
+            insert = {
+              up = "no_op";
+              down = "no_op";
+              left = "no_op";
+              right = "no_op";
+              pageup = "no_op";
+              pagedown = "no_op";
+              home = "no_op";
+              end = "no_op";
+            };
+          };
         };
         languages = [
           {
