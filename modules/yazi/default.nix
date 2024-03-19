3fa2599b0014313d580e5a9815942c545bdb8783
Simon Guest <simon.guest@agresearch.co.nz>
2024-03-20 11:56:31 +1300

    yazi config ration for columns

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -6,6 +6,11 @@
         enable = true;
         enableBashIntegration = true;
         enableNushellIntegration = true;
+        settings = {
+          manager = {
+            ratio = [1 2 5];
+          };
+        };
       };
     };
   };
