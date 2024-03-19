d252ac1f97df78e345c7d6b55e91bd9b84e8a97d
Simon Guest <simon.guest@agresearch.co.nz>
2024-03-20 09:50:37 +1300

    Add bottom

--- packages.agr.nix
+++ packages.agr.nix
@@ -5,6 +5,7 @@ with pkgs;
   # currently a big dump of what I had in nix-env
   home.packages = [
     beekeeper-studio
+    bottom
     dig
     du-dust
     emacs
--- packages.personal.nix
+++ packages.personal.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
+with pkgs;
 {
   home.packages = [
+    bottom
   ];
 }
