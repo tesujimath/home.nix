2d19243f6846e6ca87d30c157c653fe486458f56
Simon Guest <simon.guest@agresearch.co.nz>
2024-04-08 09:46:24 +1200

    Pull in common.nix into agr configs

--- home.agr-hpc.nix
+++ home.agr-hpc.nix
@@ -2,22 +2,12 @@
 
 {
   home = {
-    packages = with pkgs;
-      [
-        gcc
-      ];
-
     sessionPath = [
       "$HOME/scripts"
     ];
   };
 
-  programs = {
-    home-manager.enable = true;
-
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-  };
+  imports = [
+    ./common.nix
+  ];
 }
--- home.agr.nix
+++ home.agr.nix
@@ -1,61 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = (pkg: true);
-
   home.sessionPath = [
     "$HOME/scripts"
   ];
 
   programs = {
-    bash = {
-      enable = true;
-
-      profileExtra = ''
-        # reuse an ssh-agent if we can
-        unset SSH_AUTH_SOCK
-        for ssh_auth_sock in `ls -t /tmp/ssh-*/agent.*`; do
-          SSH_AUTH_SOCK=''$ssh_auth_sock ssh-add -l >/dev/null 2>&1
-          # 0 means connected and found ssh identities
-          # 1 means connected but no ssh identities found
-          # 2 means failed to connect
-          test $? -ne 2 && {
-            export SSH_AUTH_SOCK=$ssh_auth_sock
-            break
-          }
-        done
-        unset ssh_auth_sock
-
-        # otherwise start a new one
-        test -n "''$SSH_AUTH_SOCK" || {
-          eval `ssh-agent`
-        }
-      '';
-
-      # interactive shells only
-      initExtra = ''
-        PS1='\h\$ '
-
-        # colours for less
-        export LESS="-R"
-
-        # colours for ls
-        dircolors_config=$HOME/.dircolors
-        test -r $dircolors_config && eval $(dircolors -b $dircolors_config)
-
-        # direnv
-        eval "$(direnv hook bash)"
-      '';
-    };
-
-    home-manager.enable = true;
-
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-
     git = {
       enable = true;
       userName = "Simon Guest";
@@ -78,21 +28,13 @@
   };
 
   imports = [
-    ./packages.agr.nix
-    ./modules/helix.nix
-    ./modules/nushell
-    ./modules/tmux.nix
-    ./modules/yazi
+    ./common.nix
     ./modules/web-browser.nix
-    ./modules/zellij.nix
+    ./packages.agr.nix
   ];
 
   my.lsp = {
     # mostly they're enabled by default, except:
     terraform.enable = true;
-  };
-
-  xdg.mimeApps = {
-    enable = true;
   };
 }
