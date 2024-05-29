415fada99659cee21834bbcfbb53d4422ab62bdb
Simon Guest <simon.guest@tesujimath.org>
2024-05-30 11:36:08 +1200

    Ass some yazi keybindings as per the Yazi tips page

--- modules/yazi/default.nix
+++ modules/yazi/default.nix
@@ -11,7 +11,51 @@
             ratio = [1 2 5];
           };
         };
+
+        keymap = {
+          input = {
+            prepend_keymap = [
+              # https://yazi-rs.github.io/docs/tips#close-input-by-esc
+              {
+                on   = [ "<Esc>" ];
+                run  = "close";
+                desc = "Cancel input";
+              }
+            ];
+          };
+
+          manager = {
+            prepend_keymap = [
+              # https://yazi-rs.github.io/docs/tips#dropping-to-shell
+              {
+                on   = [ "<C-s>" ];
+                run  = "shell nu --block --confirm";
+                desc = "Open Nushell here";
+              }
+
+              # https://yazi-rs.github.io/docs/tips#smart-enter
+              # also needs smart-enter plugin, below
+              {
+                on   = [ "<Enter>" ];
+                run  = "plugin --sync smart-enter";
+                desc = "Enter the child directory, or open the file";
+              }
+            ];
+          };
+        };
       };
+    };
+
+    home.file = {
+      # https://yazi-rs.github.io/docs/tips#smart-enter
+      ".config/yazi/plugins/smart-enter.yazi/init.lua".text = ''
+          return {
+            entry = function()
+              local h = cx.active.current.hovered
+              ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+            end
+          }
+        '';
     };
   };
 }
