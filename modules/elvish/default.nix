97db5bb48b6e124363afae9c892e2142302b181a
Simon Guest <simon.guest@agresearch.co.nz>
2024-07-10 13:32:21 +1200

    Tidy direnv integration for Elvish

--- modules/elvish/default.nix
+++ modules/elvish/default.nix
@@ -26,11 +26,30 @@ with specialArgs; # for flakePkgs
 
     xdg = {
       configFile = {
-        "elvish/rc.elv".text = (builtins.readFile ./rc.elv);
+        "elvish/rc.elv".source = ./rc.elv;
       };
 
       dataFile = {
-        "elvish/lib/direnv.elv".source = ./direnv.elv;
+        "elvish/lib/direnv.elv".text = ''
+          ## hook for direnv
+          set @edit:before-readline = $@edit:before-readline {
+            try {
+              var m = [(${pkgs.direnv}/bin/direnv export elvish | from-json)]
+              if (> (count $m) 0) {
+                set m = (all $m)
+                keys $m | each { |k|
+                  if $m[$k] {
+                    set-env $k $m[$k]
+                  } else {
+                    unset-env $k
+                  }
+                }
+              }
+            } catch e {
+              echo $e
+            }
+          }
+        '';
       };
     };
   };
