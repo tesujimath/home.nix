05f7a891408d1e4af4015992a39ae650f4c3ea96
Simon Guest <simon.guest@agresearch.co.nz>
2024-07-15 11:59:11 +1200

    Switch Nix LSP to nil, and add auto-format

--- modules/helix.nix
+++ modules/helix.nix
@@ -23,35 +23,35 @@ with pkgs;
   config = {
     home.packages =
       with pkgs;
-      (if config.my.lsp.bash.enable then [nodePackages.bash-language-server shfmt] else [])
+      (if config.my.lsp.bash.enable then [ nodePackages.bash-language-server shfmt ] else [ ])
       ++
-      (if config.my.lsp.dart.enable then [dart] else [])
+      (if config.my.lsp.dart.enable then [ dart ] else [ ])
       ++
-      (if config.my.lsp.dockerfile.enable then [dockerfile-language-server-nodejs] else [])
+      (if config.my.lsp.dockerfile.enable then [ dockerfile-language-server-nodejs ] else [ ])
       ++
-      (if config.my.lsp.go.enable then [go gopls] else [])
+      (if config.my.lsp.go.enable then [ go gopls ] else [ ])
       ++
-      (if config.my.lsp.json.enable then [vscode-langservers-extracted] else [])
+      (if config.my.lsp.json.enable then [ vscode-langservers-extracted ] else [ ])
       ++
-      (if config.my.lsp.markdown.enable then [marksman] else [])
+      (if config.my.lsp.markdown.enable then [ marksman ] else [ ])
       ++
-      (if config.my.lsp.nix.enable then [nixd] else [])
+      (if config.my.lsp.nix.enable then [ nil nixpkgs-fmt ] else [ ])
       ++
-      (if config.my.lsp.packer.enable then [packer] else [])
+      (if config.my.lsp.packer.enable then [ packer ] else [ ])
       ++
-      (if config.my.lsp.python.enable then [pyright pylint black] else [])
+      (if config.my.lsp.python.enable then [ pyright pylint black ] else [ ])
       ++
-      (if config.my.lsp.rust.enable then [rust-analyzer rustfmt] else [])
+      (if config.my.lsp.rust.enable then [ rust-analyzer rustfmt ] else [ ])
       ++
-      (if config.my.lsp.terraform.enable then [terraform-ls] else [])
+      (if config.my.lsp.terraform.enable then [ terraform-ls ] else [ ])
       ++
-      (if config.my.lsp.toml.enable then [taplo-lsp] else [])
+      (if config.my.lsp.toml.enable then [ taplo-lsp ] else [ ])
       ++
-      (if config.my.lsp.typescript.enable then [nodePackages.typescript-language-server] else [])
+      (if config.my.lsp.typescript.enable then [ nodePackages.typescript-language-server ] else [ ])
       ++
-      (if config.my.lsp.typst.enable then [typst-lsp typst-fmt] else [])
+      (if config.my.lsp.typst.enable then [ typst-lsp typst-fmt ] else [ ])
       ++
-      (if config.my.lsp.yaml.enable then [yaml-language-server] else [])
+      (if config.my.lsp.yaml.enable then [ yaml-language-server ] else [ ])
     ;
 
     programs = {
@@ -77,7 +77,7 @@ with pkgs;
           };
           keys = {
             normal = {
-              "X" = ["extend_line_up" "extend_to_line_bounds"];
+              "X" = [ "extend_line_up" "extend_to_line_bounds" ];
             };
             insert = {
               up = "no_op";
@@ -91,101 +91,109 @@ with pkgs;
             };
           };
         };
-        languages = let remove-trailing-whitespace-formatter = {
-          command = "sed";
-          args = ["s/[[:space:]]*$//"];
-        }; in {
-          language = [
-            {
-              name = "nix";
-              language-servers = ["nixd"];
-            }
-            {
-              name = "python";
-              language-servers = ["pyright"];
-              formatter = { command = "black"; args = ["-" "--quiet"]; };
-              auto-format = true;
-            }
-            {
-              name = "rust";
-              formatter = { command = "rustfmt"; args = ["--edition=2021"]; };
-              auto-format = true;
-              auto-pairs = {
-                "(" = ")";
-                "{" = "}";
-                "[" = "]";
-                "\"" = "\"";
-                "`" = "`";
-              };
-            }
-            {
-              name = "bash";
-              formatter = {
-                command = "shfmt";
-                args = ["-i" "4"];
-              };
-              auto-format = true;
-            }
-            {
-              name = "dockerfile";
-              formatter = remove-trailing-whitespace-formatter;
-              auto-format = true;
-            }
-            {
-              name = "nu";
-              formatter = remove-trailing-whitespace-formatter;
-              auto-format = true;
-            }
-            {
-              name = "toml";
-              formatter = {
-                command = "taplo";
-                args = ["fmt" "-"];
-              };
-              auto-format = true;
-            }
-            {
-              name = "typst";
-              formatter = {
-                command = "typstfmt";
-                args = ["--output" "-"];
-              };
-              auto-format = true;
-            }
-            {
-              name = "yaml";
-              formatter = remove-trailing-whitespace-formatter;
-              auto-format = true;
-            }
-          ]
-          ++
-          (if config.my.lsp.packer.enable then [
-            {
-              name = "packer";
-              scope = "source.packer";
-              file-types = ["pkr.hcl"];
-              formatter = {
-                command = "packer";
-                args = ["fmt"];
-              };
-              auto-format = true;
-            }
-
-          ] else []);
-
-          language-server = {
-            nixd = {
-              command = "nixd";
+        languages =
+          let
+            remove-trailing-whitespace-formatter = {
+              command = "sed";
+              args = [ "s/[[:space:]]*$//" ];
             };
-            rust-analyzer = {
-              config = {
-                check = {
-                  command = "clippy";
+          in
+          {
+            language = [
+              {
+                name = "nix";
+                language-servers = [ "nil" ];
+                formatter = {
+                  command = "nixpkgs-fmt";
+                };
+                auto-format = true;
+              }
+              {
+                name = "python";
+                language-servers = [ "pyright" ];
+                formatter = { command = "black"; args = [ "-" "--quiet" ]; };
+                auto-format = true;
+              }
+              {
+                name = "rust";
+                formatter = { command = "rustfmt"; args = [ "--edition=2021" ]; };
+                auto-format = true;
+                auto-pairs = {
+                  "(" = ")";
+                  "{" = "}";
+                  "[" = "]";
+                  "\"" = "\"";
+                  "`" = "`";
+                };
+              }
+              {
+                name = "bash";
+                formatter = {
+                  command = "shfmt";
+                  args = [ "-i" "4" ];
+                };
+                auto-format = true;
+              }
+              {
+                name = "dockerfile";
+                formatter = remove-trailing-whitespace-formatter;
+                auto-format = true;
+              }
+              {
+                name = "nu";
+                formatter = remove-trailing-whitespace-formatter;
+                auto-format = true;
+              }
+              {
+                name = "toml";
+                formatter = {
+                  command = "taplo";
+                  args = [ "fmt" "-" ];
+                };
+                auto-format = true;
+              }
+              {
+                name = "typst";
+                formatter = {
+                  command = "typstfmt";
+                  args = [ "--output" "-" ];
+                };
+                auto-format = true;
+              }
+              {
+                name = "yaml";
+                formatter = remove-trailing-whitespace-formatter;
+                auto-format = true;
+              }
+            ]
+            ++
+            (if config.my.lsp.packer.enable then [
+              {
+                name = "packer";
+                scope = "source.packer";
+                file-types = [ "pkr.hcl" ];
+                formatter = {
+                  command = "packer";
+                  args = [ "fmt" ];
+                };
+                auto-format = true;
+              }
+
+            ] else [ ]);
+
+            language-server = {
+              nil = {
+                command = "nil";
+              };
+              rust-analyzer = {
+                config = {
+                  check = {
+                    command = "clippy";
+                  };
                 };
               };
             };
           };
-        };
       };
     };
     #home.packages = [
