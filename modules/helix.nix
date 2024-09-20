9e541c7225372f8e7cac524e4925f718df4a5f79
Simon Guest <simon.guest@tesujimath.org>
2024-09-20 14:34:57 +1200

    Helix: strip trailing whitespace in markdown files

--- modules/helix.nix
+++ modules/helix.nix
@@ -107,6 +107,11 @@ in
                 auto-format = true;
               }
               {
+                name = "markdown";
+                formatter = remove-trailing-whitespace-formatter;
+                auto-format = true;
+              }
+              {
                 name = "nu";
                 formatter = remove-trailing-whitespace-formatter;
                 auto-format = true;
