9ef1cdc9b1cc465d1c6cfc847c532445a4156061
Simon Guest <simon.guest@agresearch.co.nz>
2025-07-01 15:42:00 +1200

    Add a minimal profile for third party adoption

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 This Nix Home Manager configuration is designed to be easy to adapt for others' use.
 
-If you want to use the convenience script `home-manager-switch`, then clone this repo into `$HOME/home.nix`.
+
+## Getting Started
+
+If you want to use the convenience script `home-manager-switch`, then clone this repo into `~/home.nix`.
+
+Edit the `minimal` profile in [config/default.nix](config/default.nix) at least setting your user account
+details, and ideally giving it a better profile name.
+
+Then,
+
+```
+$ cd ~/home.nix
+$ nix run 'nixpkgs/nixpkgs-unstable#home-manager' -- switch -v --flake '.#minimal'
+```
+
+Subsequently, if you have the `bash` module enabled, the shell function `home-manager-switch` may be
+run with no arguments to reapply any changes you make to the profile.
+
+(Nix requires that any new files be added to the git index before it can read them.)
--- config/default.nix
+++ config/default.nix
@@ -47,17 +47,24 @@ let
 
   commonLanguages = allLanguages // disable [ "dart" "packer" ];
 
-  gquery-env-elvish-fn = "fn gquery-env {|env| nix run 'git+ssh://k-devops-pv01.agresearch.co.nz/tfs/Scientific/Bioinformatics/_git/gquery?ref=refs/heads/gbs_prism#export-env' -- $env}";
+  # Elvish functions shared between profiles:
+  elvish-functions =
+    {
+      gquery = "fn gquery-env {|env| nix run 'git+ssh://k-devops-elvish-functions.pv01.agresearch.co.nz/tfs/Scientific/Bioinformatics/_git/gquery?ref=refs/heads/gbs_prism#export-env' -- $env}";
 
-  mosh-eri-elvish-fn = "fn mosh-eri {|@args| e:mosh --server=/home/agresearch.co.nz/guestsi/.nix-profile/bin/mosh-server $@args}";
+      eri = "fn mosh-eri {|@args| e:mosh --server=/home/agresearch.co.nz/guestsi/.nix-profile/bin/mosh-server $@args}";
 
-  slurm-fns = ''
-    fn squeue-me { squeue --me -o "%.10A %.40j %.4t %.20S %.11M %.11L %.5m %.3c %.12B"}
-    fn squeue-all { squeue -o "%.10A %.25u %.40j %.4t %.20S %.11M %.11L %.5m %.3c %.12B"}
-    fn sacct-json {|@rest| with E:SLURM_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S" { sacct --json $@rest } }
-  '';
+      slurm = ''
+        fn squeue-me { squeue --me -o "%.10A %.40j %.4t %.20S %.11M %.11L %.5m %.3c %.12B"}
+        fn squeue-all { squeue -o "%.10A %.25u %.40j %.4t %.20S %.11M %.11L %.5m %.3c %.12B"}
+        fn sacct-json {|@rest| with E:SLURM_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S" { sacct --json $@rest } }
+      '';
+    };
 
-  moshWithKerberos = (pkgs.mosh.override { openssh = pkgs.opensshWithKerberos; }).overrideAttrs (attrs: {
+  # any profile which uses mosh with kerberos will need this:
+  moshWithKerberos = (pkgs.mosh.override {
+    openssh = pkgs.opensshWithKerberos;
+  }).overrideAttrs (attrs: {
     # The locale setting is for glibc 2.27 compatability, as per this:
     # https://github.com/NixOS/nixpkgs/issues/38991
     postFixup = ''
@@ -68,193 +75,265 @@ let
   });
 in
 {
-  agr = {
-    local = lib.attrsets.recursiveUpdate
-      (commonModules // (enable [
-        "web-browser"
-        "wezterm"
-      ]))
-      {
-        user = {
-          email = "simon.guest@agresearch.co.nz";
-          fullName = "Simon Guest";
+  # starter profile for third parties,
+  # use the real profiles below for ideas on what to tweak
+  minimal =
+    let
+      username = "CHANGEME";
+      homeDirectory = /home/CHANGEME;
+      email = "CHANGEME@CHANGEME";
+      fullName = "CHANGEME";
+    in
+    {
+      local = lib.attrsets.recursiveUpdate
+        (commonModules // (disable [
+          # list of module names (strings) of what you don't want from common modules
+        ]) // (enable [
+          # list of module names (strings) of what extra modules you want beyond common
+        ]))
+        {
+          user = {
+            inherit email;
+            inherit fullName;
+          };
+
+          languages = commonLanguages;
         };
 
-        defaultShell = "elvish";
-        defaultEditor = "hx";
+      home = {
+        inherit stateVersion;
+        inherit username;
+        inherit homeDirectory;
 
-        languages = commonLanguages;
+        packages = with pkgs; [
+          # recently Kerberos was removed from the default openssh package
+          # would be better configured via programs.ssh
+          opensshWithKerberos
+          moshWithKerberos
 
-        bash.profile.reuse-ssh-agent = true;
-
-        elvish.rcExtra = ''
-          ${gquery-env-elvish-fn}
-          ${mosh-eri-elvish-fn}
-        '';
-
-        web-browser.wsl.use-native-windows = true;
-      };
-
-    home = {
-      inherit stateVersion;
-      username = "guestsi";
-      homeDirectory = /home/guestsi;
-
-      file = {
-        ".ssh/config".source = ./dotfiles.agr/ssh_config;
-      };
-
-      sessionVariables = {
-        # in this profile I run a Windows terminal, so copy/paste must use terminal
-        FX_NO_MOUSE = "true";
-      };
-
-      packages = with pkgs; [
-        # recently Kerberos was removed from the default openssh package
-        # would be better configured via programs.ssh
-        opensshWithKerberos
-        moshWithKerberos
-      ];
-    };
-  };
-  agr-hpc = {
-    local = lib.attrsets.recursiveUpdate
-      (commonModules // (disable [
-        # use system git on legacy HPC to avoid ssh cert problem:
-        # inscrutable$ git fetch --all
-        # Fetching origin
-        # fatal: unable to access 'https://github.com/tesujimath/emacs.d.git/': OpenSSL/3.0.13: error:16000069:STORE routines::unregistered scheme
-        # error: could not fetch origin
-        "git"
-      ]))
-      {
-        user = {
-          email = "simon.guest@agresearch.co.nz";
-          fullName = "Simon Guest";
-        };
-
-        defaultShell = "elvish";
-        defaultEditor = "hx";
-
-        languages = commonLanguages;
-
-        bash.profile = {
-          reuse-ssh-agent = true;
-          conda-root = "/stash/miniconda3";
-        };
-
-        elvish.rcExtra = ''
-          ${gquery-env-elvish-fn}
-        '';
-      };
-    home = {
-      inherit stateVersion;
-      username = "guestsi";
-      homeDirectory = /home/guestsi;
-      packages = with pkgs; [
-        # recently Kerberos was removed from the default openssh package
-        # would be better configured via programs.ssh
-        opensshWithKerberos
-        moshWithKerberos
-      ];
-      sessionVariables = {
-        GIT_SSH = "/usr/bin/ssh";
+          # any extra packages you want
+        ];
       };
     };
 
-  };
-  agr-eri = {
-    local = lib.attrsets.recursiveUpdate
-      (commonModules // (disable [
-        # also use system git on eRI to avoid ssh cert problem as on legacy HPC
-        "git"
-      ]))
-      {
-        user = {
-          email = "simon.guest@agresearch.co.nz";
-          fullName = "Simon Guest";
+  agr =
+    let
+      username = "guestsi";
+      homeDirectory = /home/guestsi;
+      email = "simon.guest@agresearch.co.nz";
+      fullName = "Simon Guest";
+    in
+    {
+      local = lib.attrsets.recursiveUpdate
+        (commonModules // (enable [
+          "web-browser"
+          "wezterm"
+        ]))
+        {
+          user = {
+            inherit email;
+            inherit fullName;
+          };
+
+          defaultShell = "elvish";
+          defaultEditor = "hx";
+
+          languages = commonLanguages;
+
+          bash.profile.reuse-ssh-agent = true;
+
+          elvish.rcExtra = ''
+            ${elvish-functions.gquery}
+            ${elvish-functions.eri}
+          '';
+
+          web-browser.wsl.use-native-windows = true;
         };
 
-        defaultShell = "elvish";
-        defaultEditor = "hx";
+      home = {
+        inherit stateVersion;
+        inherit username;
+        inherit homeDirectory;
 
-        languages = commonLanguages;
+        file = {
+          ".ssh/config".source = ./dotfiles.agr/ssh_config;
+        };
 
-        bash.profile = {
-          reuse-ssh-agent = true;
-          conda-root = "/agr/persist/apps/eri_rocky8/software/Miniforge3/24.9.0-0";
-          extra = ''
-            # work-around for ssh-add: No user found with uid:
-            export LD_PRELOAD=/usr/lib64/libnss_sss.so.2
+        sessionVariables = {
+          # in this profile I run a Windows terminal, so copy/paste must use terminal
+          FX_NO_MOUSE = "true";
+        };
+
+        packages = with pkgs; [
+          # recently Kerberos was removed from the default openssh package
+          # would be better configured via programs.ssh
+          opensshWithKerberos
+          moshWithKerberos
+        ];
+      };
+    };
+
+  agr-hpc =
+    let
+      username = "guestsi";
+      homeDirectory = /home/guestsi;
+      email = "simon.guest@agresearch.co.nz";
+      fullName = "Simon Guest";
+    in
+    {
+      local = lib.attrsets.recursiveUpdate
+        (commonModules // (disable [
+          # use system git on legacy HPC to avoid ssh cert problem:
+          # inscrutable$ git fetch --all
+          # Fetching origin
+          # fatal: unable to access 'https://github.com/tesujimath/emacs.d.git/': OpenSSL/3.0.13: error:16000069:STORE routines::unregistered scheme
+          # error: could not fetch origin
+          "git"
+        ]))
+        {
+          user = {
+            inherit email;
+            inherit fullName;
+          };
+
+          defaultShell = "elvish";
+          defaultEditor = "hx";
+
+          languages = commonLanguages;
+
+          bash.profile = {
+            reuse-ssh-agent = true;
+            conda-root = "/stash/miniconda3";
+          };
+
+          elvish.rcExtra = ''
+            ${elvish-functions.gquery}
+          '';
+        };
+      home = {
+        inherit stateVersion;
+        inherit username;
+        inherit homeDirectory;
+        packages = with pkgs; [
+          # recently Kerberos was removed from the default openssh package
+          # would be better configured via programs.ssh
+          opensshWithKerberos
+          moshWithKerberos
+        ];
+        sessionVariables = {
+          GIT_SSH = "/usr/bin/ssh";
+        };
+      };
+
+    };
+
+  agr-eri =
+    let
+      username = "guestsi@agresearch.co.nz";
+      homeDirectory = /home/agresearch.co.nz/guestsi;
+      email = "simon.guest@agresearch.co.nz";
+      fullName = "Simon Guest";
+    in
+    {
+      local = lib.attrsets.recursiveUpdate
+        (commonModules // (disable [
+          # also use system git on eRI to avoid ssh cert problem as on legacy HPC
+          "git"
+        ]))
+        {
+          user = {
+            inherit email;
+            inherit fullName;
+          };
+
+          defaultShell = "elvish";
+          defaultEditor = "hx";
+
+          languages = commonLanguages;
+
+          bash.profile = {
+            reuse-ssh-agent = true;
+            conda-root = "/agr/persist/apps/eri_rocky8/software/Miniforge3/24.9.0-0";
+            extra = ''
+              # work-around for ssh-add: No user found with uid:
+              export LD_PRELOAD=/usr/lib64/libnss_sss.so.2
+            '';
+          };
+
+          elvish.rcExtra = ''
+            ${elvish-functions.gquery}
+            ${elvish-functions.slurm}
           '';
         };
 
-        elvish.rcExtra = ''
-          ${gquery-env-elvish-fn}
-          ${slurm-fns}
-        '';
+      home = {
+        inherit stateVersion;
+        inherit username;
+        inherit homeDirectory;
+        packages = [
+          # don't use Nix ssh, but we need mosh for mosh-server
+          moshWithKerberos
+        ];
+        sessionVariables = { };
       };
-
-    home = {
-      inherit stateVersion;
-      username = "guestsi@agresearch.co.nz";
-      homeDirectory = /home/agresearch.co.nz/guestsi;
-      packages = [
-        # don't use Nix ssh, but we need mosh for mosh-server
-        moshWithKerberos
-      ];
-      sessionVariables = { };
     };
-  };
-  personal = {
-    local = lib.attrsets.recursiveUpdate
-      (commonModules // (enable [
-        "ledger"
-        "syncthing"
-        "web-browser"
-        "xmonad-desktop"
-      ]))
-      {
-        user = {
-          email = "simon.guest@tesujimath.org";
-          fullName = "Simon Guest";
-        };
 
-        defaultShell = "elvish";
-        defaultEditor = "hx";
-
-        languages = commonLanguages;
-      };
-
-    home = {
-      inherit stateVersion;
+  personal =
+    let
       username = "sjg";
       homeDirectory = /home/sjg;
+      email = "simon.guest@tesujimath.org";
+      fullName = "Simon Guest";
+    in
+    {
+      local = lib.attrsets.recursiveUpdate
+        (commonModules // (enable [
+          "ledger"
+          "syncthing"
+          "web-browser"
+          "xmonad-desktop"
+        ]))
+        {
+          user = {
+            inherit email;
+            inherit fullName;
+          };
 
-      file = {
-        ".env.sh".source = ./dotfiles.personal/env.sh;
-        ".ssh/config".source = ./dotfiles.personal/ssh_config;
+          defaultShell = "elvish";
+          defaultEditor = "hx";
+
+          languages = commonLanguages;
+        };
+
+      home = {
+        inherit stateVersion;
+        inherit username;
+        inherit homeDirectory;
+
+        file = {
+          ".env.sh".source = ./dotfiles.personal/env.sh;
+          ".ssh/config".source = ./dotfiles.personal/ssh_config;
+        };
+        packages = with pkgs;
+          [
+            _1password-cli
+            audacious
+            bind
+            calibre
+            freerdp
+            gimp
+            git-crypt
+            git-imerge
+            ijq
+            jo
+            nodejs
+            python3
+            # stow # was for install-dotfiles
+            unison
+            vscode
+            yarn
+            zoom-us
+          ];
       };
-      packages = with pkgs;
-        [
-          _1password-cli
-          audacious
-          bind
-          calibre
-          freerdp
-          gimp
-          git-crypt
-          git-imerge
-          ijq
-          jo
-          nodejs
-          python3
-          # stow # was for install-dotfiles
-          unison
-          vscode
-          yarn
-          zoom-us
-        ];
     };
-  };
 }
