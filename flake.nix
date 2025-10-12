64653e72e55608fe1a4b061afabf647e4bc385a0
Simon Guest <simon.guest@agresearch.co.nz>
2025-10-13 11:12:49 +1300

    Use github:NixOS/nixpkgs/25.05 for Nix 2.17 on eRI
    gc

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Nix flake for Nix Home Manager config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
